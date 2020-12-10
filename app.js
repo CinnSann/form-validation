c9802b5f09ca838a13d3edfc9e42d7abf8322fc9
CinnSann <cinnssann729@gmail.com>
2020-12-10 16:18:40 -0600

    adds validation for email and password

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const regForm = document.getElementById('registration-form')
 const username = document.getElementById('username')
 const usernameErr = username.nextElementSibling
+const email = document.getElementById('email')
+const emailErr = email.nextElementSibling
+const password = document.getElementById('password')
+const passwordErr = password.nextElementSibling
+// const email = document.getElementById('email')
 // const usernameErr = document.getElementById("usernameErr")
 
 // 2. Add event / modification
@@ -18,7 +23,17 @@ regForm.addEventListener('click', function (e) {
   }
 
   //@TODO: add the validation for email
-
+  if (email.value === '') {
+    // 3. If empty, give user some feedback
+    emailErr.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  }
   //@TODO: add the validation for password
-
+  if (password.value === '') {
+    // 3. If empty, give user some feedback
+    passwordErr.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  }
 })
