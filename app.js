355bd06312ed44ec0bcb2a14bfd9c8cd0b4278c7
CinnSann <cinnsann729@gmail.com>
2021-01-10 22:29:58 -0600

    adds styles, validates minLength

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // 1. get DOM element
 const form = document.querySelector('#form')
-const username = document.querySelector('#username')
-const email = document.querySelector('#email')
-const password = document.querySelector('#password')
+// const username = document.querySelector('#username')
+// const email = document.querySelector('#email')
+// const password = document.querySelectorAll('password')
 
 
 
@@ -11,17 +11,14 @@ form.addEventListener('submit', function (e) {
 
   validateEmpty(username)
   validateEmpty(email)
-  // validateEmpty(password)
-  // validateMinLength(username)
-  // validateMinLength(username)
-  // validateFirstName(firstName)
-  validateIsEmail(email)
+  validateEmpty(password)
+  validateEmpty(password2)
+  validateMinLength(username)
+  validateMinLength(password)
+  validateMinLength(password2)
+  // validatePassMatch(password, password2)
+  // validateIsEmail(email)
 
-  //@TODO: add the validation for email (Code Challenge 5a)
-  //@TODO: Code Challenge 5b: Refactor your CC 5a to use function with the "blueprints" below
-
-  //@TODO: add the validation for password (Code Challenge 5a)
-  //@TODO: Code Challenge 5b: Refactor your CC 5a to use function with the "blueprints" below
   //@TODO: Code Challenge 5c: Starting from your refactored code,
    // 1. Implement and use the validatePassMatch() function to confirm both passwords match
    // 2. Implement and use the emailValidates() function to confirm it's a valid email. See: https://stackoverflow.com/a/46181
@@ -30,54 +27,54 @@ form.addEventListener('submit', function (e) {
 
 function validateEmpty(input) {
   const inputName = input.name
+  // const msg = 'inputName is required!'
   if (input.value === '') {
-    showError(input, `${inputName} is required` )
+    showError(input) // establish msg as a variable w. const @ 2:37
   } else {
     showSuccess(input)
   }
 }
 
-// input: HTMLInputElement (DOM) | msg: string
-function showError(input, msg) {
-  console.log(input)
-  console.log(nextElementSibling)
-  input.nextElementSibling.innerHTML = `<small class="error">${msg}</small>`
- 
-  // reverts the success border
+// // input: HTMLInputElement (DOM) | msg: string
+function showError(input) {
+  // console.log(input)
+  // console.log(nextElementSibling)
+  input.nextElementSibling.innerHTML = `<small class="error">Field is required!</small>`
   input.className='base-input'
+  // reverts the success border
 }
 
-function showSuccess (input, msg) {
+function showSuccess (input) {
   input.className = 'success-input'
   // removes the flash message
-  input.nextElementSibling.innerHTML = '<small class="error">${msg}</small>'
+  input.nextElementSibling.innerHTML = `<small class="success">Accepted!</small>`
 }
 
-function validatePassMatch(password1, password2){
-  // password1.value === password2.value
-  //@TODO: check if the passwords match
-}
+// function validatePassMatch(password1, password2){
+//   // password1.value === password2.value
+//   //@TODO: check if the passwords match
+// }
 
-function validateIsEmail(email){
-  if (emailValidates(email) {
-    showSuccess(email)
-  } else {
-    showError(email, msg)
-  }
-}
+// function validateIsEmail(email){
+//   if (emailValidates(email) {
+//     showSuccess(email)
+//   } else {
+//     showError(email, msg)
+//   }
+// }
 
-// returns boolean
-function emailValidates (email) {
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// // returns boolean
+// function emailValidates (email) {
+//   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   
-  return true
-}
+//   return true
+// }
 
-// input: HTMLInputElement
+// // input: HTMLInputElement
 function validateMinLength(input) {
-  // console.log(input.value.length)
+  console.log(input.value.length)
   if (input.value.length < 7 || input.value.length > 15) {
-    showError(input, msg:'Username is too short')
+    showError(input)
   } else {
     showSuccess(input)
   }
