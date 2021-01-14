2b278fbe3e2b29e53c68afe6396b10a9556159da
CinnSann <cinnsann729@gmail.com>
2021-01-14 11:44:24 -0600

    validate length

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // 1. get DOM element
 const form = document.querySelector('#form')
-// const username = document.querySelector('#username')
-// const email = document.querySelector('#email')
-// const password = document.querySelectorAll('password')
+const username = document.querySelector('#username')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
 
 
 
@@ -12,12 +12,9 @@ form.addEventListener('submit', function (e) {
   validateEmpty(username)
   validateEmpty(email)
   validateEmpty(password)
-  validateEmpty(password2)
-  validateMinLength(username)
-  validateMinLength(password)
-  validateMinLength(password2)
+  validateMinLength(username, password, password2)
   // validatePassMatch(password, password2)
-  // validateIsEmail(email)
+  ValidateisEmail(email)
 
   //@TODO: Code Challenge 5c: Starting from your refactored code,
    // 1. Implement and use the validatePassMatch() function to confirm both passwords match
@@ -27,53 +24,55 @@ form.addEventListener('submit', function (e) {
 
 function validateEmpty(input) {
   const inputName = input.name
-  // const msg = 'inputName is required!'
   if (input.value === '') {
-    showError(input) // establish msg as a variable w. const @ 2:37
+    showError(input, `${input.name} is required`) // establish msg as a variable w. const @ 2:37
   } else {
     showSuccess(input)
   }
 }
 
-// // input: HTMLInputElement (DOM) | msg: string
-function showError(input) {
+
+// input: HTMLInputElement (DOM) | msg: string
+function showError(input, msg) {
   // console.log(input)
   // console.log(nextElementSibling)
-  input.nextElementSibling.innerHTML = `<small class="error">Field is required!</small>`
+  input.nextElementSibling.innerHTML = `<small class="error">${input.name} is required!</small>`
   input.className='base-input'
-  // reverts the success border
 }
 
 function showSuccess (input) {
   input.className = 'success-input'
-  // removes the flash message
-  input.nextElementSibling.innerHTML = `<small class="success">Accepted!</small>`
+  input.nextElementSibling.innerHTML = `<small class="success">accepted!</small>`
 }
 
-// function validatePassMatch(password1, password2){
-//   // password1.value === password2.value
+// function validatePassMatch(password, password2) {
+//   password.value === password2.value
+  
 //   //@TODO: check if the passwords match
 // }
 
-// function validateIsEmail(email){
-//   if (emailValidates(email) {
-//     showSuccess(email)
-//   } else {
-//     showError(email, msg)
-//   }
-// }
 
-// // returns boolean
-// function emailValidates (email) {
-//   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  
-//   return true
-// }
+
+function validateIsEmail(email){
+const emailName = email.value
+  if (emailValidates(email.value.match)) {
+    showSuccess(email + `is valid`)
+  } else {
+    showError(email + `email is invalid`)
+  }
+
+}
+// return boolean
+function emailValidates (email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return true
+}
+
 
 // // input: HTMLInputElement
 function validateMinLength(input) {
   console.log(input.value.length)
-  if (input.value.length < 7 || input.value.length > 15) {
+  if (input.value.length < 7) {
     showError(input)
   } else {
     showSuccess(input)
